bac2a2d53dc74dd1098286b17f8c7432eb57ffd6
FranklinSaad <franklinpinheirosaad@hotmail.com>
2020-09-03 22:19:25 -0300

    inicial commit

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const modalOverlay = document.querySelector('.modalOverlay');
 const foods = document.querySelectorAll('.food')
 
-for (let food of foods){
-    food.addEventListener("click", function(){
+for (let food of foods) {
+    food.addEventListener("click", function () {
         const imgID = food.getAttribute("id");
         const foodname = food.querySelector(".food-name").textContent;
         const foodauthor = food.querySelector(".food-author").textContent;
-        
+
         modalOverlay.classList.add('active');
         modalOverlay.querySelector("img").src = `${imgID}`;
         modalOverlay.querySelector(".modalPlate").innerHTML = `${foodname}`;
         modalOverlay.querySelector(".modalChef").innerHTML = `${foodauthor}`;
-    
-    
     })
 }
 
 
-document.querySelector(".closeModal").addEventListener("click", function(){
+document.querySelector(".closeModal").addEventListener("click", function () {
     modalOverlay.classList.remove("active")
 })
